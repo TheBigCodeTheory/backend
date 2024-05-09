f8dcf17ea25d9f42c7eaa7d92e55fe8b5611a226
Sofia Vigna <sofiavign@gmail.com>
2024-05-09 18:10:45 -0300

    get me

--- src/resources/user/user.controller.ts
+++ src/resources/user/user.controller.ts
@@ -1,6 +1,18 @@
-import { Controller, Param, Patch, Version } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Request,
+  UseGuards,
+  Version,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { ObjectId } from 'mongoose';
+import { JwtAuthGuard } from '../auth/passport/jwt-auth.guard';
+import { RolesGuard } from 'src/lib/security/roles.guard';
+import { Roles } from 'src/lib/security/roles.decorator';
+import { ROLE } from 'src/lib/common/types';
 
 @Controller('user')
 export class UserController {
@@ -10,5 +22,13 @@ export class UserController {
   @Patch('/admin/:id')
   makeAdmin(@Param('id') id: ObjectId) {
     return this.userService.makeAdmin(id);
+  }
+
+  @Version('1')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles([ROLE.USER])
+  @Get('/me')
+  getMe(@Request() req) {
+    return this.userService.findById(req.user.id);
   }
 }
