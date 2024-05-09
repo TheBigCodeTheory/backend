742fea3a65b14ef9516788aca4479c07744e57e2
Eze <ezequiel.n.villa@gmail.com>
2024-05-09 12:13:11 -0300

    New: user endpoint to make a user an admin user to

--- src/resources/user/user.controller.ts
+++ src/resources/user/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Patch, Version } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ObjectId } from 'mongoose';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Version('1')
+  @Patch('/admin/:id')
+  makeAdmin(@Param('id') id: ObjectId) {
+    return this.userService.makeAdmin(id);
+  }
 }
